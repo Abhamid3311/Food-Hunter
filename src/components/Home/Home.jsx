fb0fd478de8e34095da8877c67a5a53009fd5365
Hamid <www.abdulhamid3311@gmail.com>
2025-02-10 22:11:57 +0600

    Setup Tailwind, React Router & Daisy UI Done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,10 @@ const Home = () => {
         <div>
 
             <h1 className="text-3xl font-bold underline">This is Home Compo</h1>
+
+            <button className="btn btn-primary">Primary</button>
+            <button className="btn btn-secondary">Secondary</button>
+            <button className="btn btn-accent">Accent</button>
         </div>
     );
 };

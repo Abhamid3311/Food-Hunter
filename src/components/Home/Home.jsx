31955bb537a958430f72e8f4523a7704384eed3d
Hamid <www.abdulhamid3311@gmail.com>
2025-02-12 21:54:40 +0600

    React Icon Added & Navbar Done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,9 @@
 
 const Home = () => {
     return (
-        <div className="bg-bgClr h-[100vh] w-full">
+        <div className=" w-full">
+            <h1>Home</h1>
 
-            <h1 className="text-3xl font-bold underline">This is Home Compo</h1>
-
-            <h3>This is Header For Hunter</h3>
-
-            <button className="btn btn-primary">Primary</button>
-            <button className="btn btn-secondary">Secondary</button>
-            <button className="btn btn-accent">Accent</button>
         </div>
     );
 };
--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,14 +1,118 @@
+import { FaClock } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoMdCart } from "react-icons/io";
+
+
+
 
 const Header = () => {
     return (
-        <div>
-            <div className="bg-primaryRed px-20 py-2 text-TextWhite w-full">
-                <h1>01364136464</h1>
+        <div className="shadow-lg">
+            <div>
+                <div className="bg-primaryRed px-20 py-2 text-TextWhite w-full flex items-center justify-between text-[12px]">
+                    <div className="flex items-center gap-10">
+                        <p className="flex items-center justify-center gap-2">
+                            <FaClock className="text-lg" />
+                            <span>7.30 AM - 9.30 PM </span>
+                        </p>
+
+                        <p className="flex items-center justify-center gap-2">
+                            <FaPhoneAlt className="text-lg" />
+                            <span>+880 1630 225 015 </span>
+                        </p>
+                    </div>
+                    <button className="font-bold">Register</button>
+                </div>
             </div>
 
 
-            <h3>This is Navbar</h3>
+            <div className="navbar bg-bgClr shadow-lg">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a>Home</a></li>
+                            <li><a>About</a></li>
+                            <li>
+                                <a>Items</a>
+                                <ul className="p-2">
+                                    <li><a>Dinner</a></li>
+                                    <li><a>Lunch</a></li>
+                                    <li><a>Breakfast</a></li>
+                                    <li><a>fastfood</a></li>
+                                    <li><a>DesiFood</a></li>
+                                </ul>
+                            </li>
+                            <li><a>Contacts</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">
+                        <img src="/src/assets/logo.png" alt="logo" className="w-[90px] h-[50px]" />
+                    </a>
+                </div>
+
+
+                <div className="navbar-center hidden lg:flex px-20  ">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li>
+                            <details>
+                                <summary>Items</summary>
+                                <ul className="p-2">
+                                    <li><a>Dinner</a></li>
+                                    <li><a>Lunch</a></li>
+                                    <li><a>Breakfast</a></li>
+                                    <li><a>fastfood</a></li>
+                                    <li><a>DesiFood</a></li>
+                                </ul>
+                            </details>
+                        </li>
+                        <li><a>Contacts</a></li>
+                        <li><a className="text-TextWhite bg-secondaryGray px-2 py-2 rounded-full ">
+                            <IoMdCart className="text-2xl" />
+                        </a></li>
+                    </ul>
+                </div>
+
+                <div className="navbar-end">
+
+                    <div className="hidden lg:flex items-center gap-5 mr-10 ">
+                        <div className="text-sm ">
+                            <p className="font-bold">Delivery Order</p>
+                            <p>+880 1630 225 015</p>
+                        </div>
+                        <img src="/src/assets/food 1.png" alt="food-delivery" className="w-[35px] h-[35px]" />
+                    </div>
+
+                    <a className=" bg-primaryRed px-8 py-2 rounded-md font-bold text-TextWhite text-center">
+                        <button>Login</button>
+                    </a>
+                </div>
+            </div>
+
+
+
         </div>
+
+
+
+
     );
 };
 

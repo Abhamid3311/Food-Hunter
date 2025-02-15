959adcb2307971442f7f293fd906c07e666badfe
Hamid <www.abdulhamid3311@gmail.com>
2025-02-15 12:00:31 +0600

    HEALTHY section done

--- src/components/Home/HighestQuality.jsx
+++ src/components/Home/HighestQuality.jsx
@@ -16,17 +16,17 @@ const HighestQuality = () => {
                     <img src="/src/assets/home_img/image_richHealthy_1.png" alt="" />
                 </div>
                 <div>
-                    <h1 className="text-3xl font-bold">Highest quality artisangrains, proteins br & seasonal ingredients</h1>
+                    <h1 className="text-3xl font-bold ">Highest quality artisangrains, proteins & <br /> seasonal ingredients</h1>
 
-                    <p className=" text-secondaryGray w-[550px] mt-5">Righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desires, that they cannot foresee.</p>
+                    <p className=" text-secondaryGray  mt-5">Righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desires, that they cannot foresee.</p>
 
-                    <div className="flex items-center justify-between  gap-3">
-                        <div className="text-secondaryGray">
+                    <div className="flex items-center justify-between   gap-3">
+                        <div className="text-secondaryGray text-lg gap-3">
                             <p className="flex items-center justify-start gap-2">
                                 <FaStar className="text-primaryRed text-lg font-bold" />
                                 <span> Simple and easy to distinguish</span>
                             </p>
-                            <p className="flex items-center justify-start gap-2">
+                            <p className="flex items-center justify-start gap-2 py-3">
                                 <FaStar className="text-primaryRed text-lg font-bold" />
                                 <span> Pleasure of the momentblinded desire</span>
                             </p>
@@ -34,14 +34,17 @@ const HighestQuality = () => {
                                 <FaStar className="text-primaryRed text-lg font-bold" />
                                 <span> Able to do what we like best</span>
                             </p>
-
-                          
-
+                            <p className="flex items-center justify-start gap-2 py-3">
+                                <FaStar className="text-primaryRed text-lg font-bold" />
+                                <span> Able to do what we like best</span>
+                            </p>
+                            <p className="flex items-center justify-start gap-2">
+                                <FaStar className="text-primaryRed text-lg font-bold" />
+                                <span> Able to do what we like best</span>
+                            </p>
                         </div>
 
-                        <div>
-                            <img src="/src/assets/home_img/image_richHealthy_2.png" alt="" />
-                        </div>
+                        <img src="/src/assets/home_img/image_richHealthy_2.png" alt="" />
 
                     </div>
 

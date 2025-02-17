121201e1f8e650031191043e6fbaa3e34ab30fe9
Hamid <www.abdulhamid3311@gmail.com>
2025-02-17 22:27:42 +0600

    Added context API

--- src/components/Home/SpecialsFoodMenu.jsx
+++ src/components/Home/SpecialsFoodMenu.jsx
@@ -1,17 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { FoodContext } from "../Layouts/Layouts";
 
 const SpecialsMenu = () => {
-    const [allFoods, setAllFoods] = useState([]);
+    const allFoods = useContext(FoodContext);
 
-    useEffect(() => {
-        fetch("mealItems.json")
-            .then((res) => res.json())
-            .then((data) => setAllFoods(data))
-
-    }, [])
 
     console.log(allFoods)
-
 
 
     return (
@@ -21,42 +15,28 @@ const SpecialsMenu = () => {
                 <h1 className="text-2xl lg:text-4xl font-bold text-secondaryGray mb-2">Check out our menu</h1>
                 <p className="text-secondaryGray">Demoralized by the charms of pleasure of the moment so blinded except to some advantage.</p>
 
-                <div className="grid grid-cols-4 gap-5 mt-10">
-                    {
-                        allFoods.map((food) => <div key={food?.id} className=" w-full h-full bg-TextWhite shadow-xl rounded-md object-cover">
+                {
+                    allFoods.length !== 0 ?
+                        <div className="grid grid-cols-4 gap-5 mt-10">
+                            {
+                                allFoods.map((food) => <div key={food?.id} className=" w-full h-full bg-TextWhite shadow-xl rounded-md object-cover">
 
-                            <img src={food.img || "https://via.placeholder.com/300"} alt={food.name} className="w-full h-[250px] rounded-t-md" />
+                                    <img src={food.img || "https://via.placeholder.com/300"} alt={food.name} className="w-full h-[250px] rounded-t-md" />
 
-                            <div className="card-body p-4 text-start text-secondaryGray">
-                                <h1 className="text-2xl font-bold "> {food?.name}</h1>
-                                <p>Category: {food?.category}</p>
-                                <p className="text-primaryRed font-bold">Price: {food?.price}</p>
+                                    <div className="card-body p-4 text-start text-secondaryGray">
+                                        <h1 className="text-2xl font-bold "> {food?.name}</h1>
+                                        <p>Category: {food?.category}</p>
+                                        <p className="text-primaryRed font-bold">Price: {food?.price}</p>
 
-                                <div className="card-actions justify-end">
-                                    <div className="badge badge-outline">View Details</div>
-                                    <div className="badge badge-outline">Add To Cart</div>
-                                </div>
-                            </div>
-
-
-
-                            {/*   <figure>
-                               
-                            </figure>
-                            <div >
-                                <h2 className="card-title">
-                                   
-                                    <div className="badge badge-secondary">NEW</div>
-                                </h2>
-                                <p>{food?.category}</p>
-                                <p className="text-primaryRed">{food?.price}</p>
-                                <div >
-                                  
-                                </div>
-                            </div> */}
-                        </div>)
-                    }
-                </div>
+                                        <div className="card-actions justify-end">
+                                            <div className="badge badge-outline">View Details</div>
+                                            <div className="badge badge-outline">Add To Cart</div>
+                                        </div>
+                                    </div>
+                                </div>)
+                            }
+                        </div> : "Loading...."
+                }
             </div>
         </div>
     );
--- src/components/Layouts/Layouts.jsx
+++ src/components/Layouts/Layouts.jsx
@@ -1,18 +1,35 @@
 import Header from './Header';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
+import { createContext, useEffect, useState } from 'react';
+
+export const FoodContext = createContext(null)
 
 const Layouts = () => {
+
+    const [allFoods, setAllFoods] = useState([]);
+
+    useEffect(() => {
+        fetch("mealItems.json")
+            .then((res) => res.json())
+            .then((data) => setAllFoods(data))
+
+    }, [])
+
+    // console.log(allFoods);
+
     return (
-        <div className='w-full'>
-            <Header />
+        <FoodContext.Provider value={allFoods}>
+            <div className='w-full'>
+                <Header />
 
-            <div>
-                <Outlet />
+                <div>
+                    <Outlet />
+                </div>
+
+                <Footer />
             </div>
-
-            <Footer />
-        </div>
+        </FoodContext.Provider>
     );
 };
 
